a3ae8bda7dcb25a039fff6896b4fb68e6fca3c57
Xplozionggwp <89824888+Xplozionggwp@users.noreply.github.com>
2023-08-15 20:15:48 +0530

    Add files via upload

---
+++ index.php
@@ -0,0 +1,54 @@
+  <!DOCTYPE html>
+  <html lang="en">
+  <head>
+    <title>Employee Management System</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <!-- Main CSS-->
+    <link rel="stylesheet" type="text/css" href="css/main.css">
+    <!-- Font-icon css-->
+    <link rel="stylesheet" type="text/css" href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css">
+    <style>
+      /* Logo styles */
+      .logo-container {
+        text-align: center;
+        margin-top: 20px; /* Adjust the margin as needed */
+      }
+      
+      .logo {
+        max-width: 100px; /* Adjust the maximum width for the smaller logo */
+        height: auto;
+      }
+    </style>
+  </head>
+  <body>
+    <!-- Navbar-->
+    <main>
+      <div class="page-error tile">
+        <!-- Logo container -->
+        <div class="logo-container">
+          <img class="logo" src="logo.jpg" alt="Logo">
+        </div>
+
+        <!-- Other content -->
+        <img src='frontpage.png' height="400" width="800">
+        <h1><i class="fa fa-exclamation-circle"></i> TimelyHR - Leave management system</h1>
+        <h3>From absence to balance timelyHR's magic!</h3>
+        <p>
+        <p><a class="btn btn-primary btn-lg" href="emp/">I'm an Employee</a>
+          &nbsp;&nbsp;
+          <a class="btn btn-success btn-lg" href="admin/">I'm an Admin</a></p>
+      </div>
+    </main>
+    <!-- Essential javascripts for application to work-->
+    <script src="js/jquery-3.3.1.min.js"></script>
+    <script src="js/popper.min.js"></script>
+    <script src="js/bootstrap.min.js"></script>
+    <script src="js/main.js"></script>
+    <!-- The javascript plugin to display page loading on top-->
+    <script src="js/plugins/pace.min.js"></script>
+    <!-- Page specific javascripts-->
+    <!-- Google analytics script-->
+  </body>
+  </html>
